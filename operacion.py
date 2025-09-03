497e5b8d30692ba5b33647e1b54e9aefe5efc10c
elvis <elvishuaynoca@gmail.com>
2025-09-03 11:34:27 -0400

    mi archivo operacion python

---
+++ operacion.py
@@ -0,0 +1,9 @@
+#operacion simpple 
+numero1 = 5
+numero2 = 10
+suma = numero1 + numero2
+print("La suma de", numero1, "y", numero2, "es:", suma)
+print("Elvis Erik Huaynoca Condori")
+print("Josema Cedrik Cruz Ramos")
+print("Paolo Carlos Cordero Rojas")
+print("Pablo Sandro Butron Garay")
