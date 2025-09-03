b21617413006a223a838360ad726d13cdfbd2ceb
pcordero-U <pcordero@fcpn.edu.bo>
2025-09-03 11:28:04 -0400

    mi archivo saludo java

---
+++ Saludo.java
@@ -0,0 +1,13 @@
+
+public class Saludo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("¡Hola equipo!");
+		System.out.println("Paolo Carlos Cordero Rojas");//desarollador Saludo.java
+		System.out.println("Josema Cedrik Cruz Ramos");// desarollo de html
+		System.out.println("Elvis Erik Huaynoca Condori");	//desarollo de python
+		System.out.println("Pablo Sandro Butron Garay");	//lider desarollo del repositorio github
+	}
+
+}
